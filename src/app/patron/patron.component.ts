34127d52a1e673584cb4eaf06f69d5fac439364b
Student <example@example.com>
2019-06-13 11:39:48 -0700

    add styling and structure to patron component

--- src/app/patron/patron.component.ts
+++ src/app/patron/patron.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DataService } from "../data.service";
-import { Taproom } from '../tapRoom';
+import { Taproom, Keg } from '../tapRoom';
 
 
 @Component({
@@ -12,18 +12,23 @@ import { Taproom } from '../tapRoom';
 export class PatronComponent implements OnInit {
   tapRoom: Taproom;
   thing;
-  constructor(private data: DataService) { }
+  kegs: Keg[] = [];
+  constructor(private data: DataService) { 
+    this.kegs.push(
+      new Keg('Number Six Saison', 'Battlebar Beerlactica', 6, 124, 66, 'This full-bodied saison may lead you to bad decisions.'), 
+      new Keg('Skin Job Stout', 'Battlebar Beerlactica', 8.9, 72, 100, 'Made from 100% synthetic components, this smooth stout with surprise you'), 
+      new Keg('Adama Pale Ale', 'Battlebar Beerlactica', 5.5, 40, 23, 'This traditional ale will command your taste buds'), 
+      new Keg('The Wheat-eenth Colony', 'Battlebar Beerlactica', 7, 103, 43, 'This illusive wheat ale is a great way to end the night, if you can find it'), 
+      new Keg('Col. Tigh Rye', 'Battlebar Beerlactica', 12, 9, 98, 'This rye, aged in bourban barrels for an unknown abount of time, will declare marshall law on your tastebuds, and affect your depth perception.'))
+
+  }
 
   ngOnInit() {
     this.data.currentTaproom.subscribe(tapRoom => this.tapRoom = tapRoom)
   }
 
-  dostuff() {
-    console.log(this.thing);    
-  }
 
-  show() {
-    //this.data.updateTaproom
-  }
 
+
+  
 }
